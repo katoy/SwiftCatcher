0a3150c3647cc51397337b768aded194201acdd5
katoy <youichikato@gmail.com>
2015-05-02 23:20:33 +0900

    Initial Commit

---
+++ SwiftCatcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCatcher
+//
+//  Created by katoy on 2015/05/02.
+//  Copyright (c) 2015年 Youichi Kato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
